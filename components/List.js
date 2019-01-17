e9ff185955e49fb4395bcf54673ed372dc98de15
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-01-17 20:53:09 +0700

    Perbaikan Proses Read

--- components/List.js
+++ components/List.js
@@ -4,20 +4,43 @@ import { Text, Button, Card, Icon, ListItem } from 'react-native-elements';
 import firebase from '../firebase';
 
 class List extends React.Component {
-
-    render(){
+    render(){   
         const { list, navigate } = this.props
         return(
-            <View>
-                <View style={{width: 250}}>
-                    <ListItem
-                        key={list.key}
-                        title={list.text}
-                    />
-                </View>
+            <View style={styles.todoList}>
+            <Icon name='edit'
+              type='materialIcons'
+              color='#4CAF50'
+              onPress={() => navigate.navigate("EditPage", {todoList: list})}
+
+            />
+
+            <Text h1>|</Text>
+
+            <TouchableNativeFeedback>
+            <Icon name='delete'
+              type='materialIcons'
+              color='#FF5722'
+              onPress={() => this.deleteTodo(list.key)}
+            />
+            </TouchableNativeFeedback>
+
+            <View style={{width: 250}}>
+              <ListItem
+                key={list.key}
+                title={list.text}
+              />
             </View>
+          </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    todoList: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+});
 
 export default List
--- components/TodoList.js
+++ components/TodoList.js
@@ -1,19 +1,76 @@
 import React from 'react';
 import { StyleSheet, View, FlatList, Alert, ActivityIndicator, Text, ScrollView } from 'react-native';
-import { Card, Button, Icon } from 'react-native-elements'
+import { Card, Button, Icon } from 'react-native-elements';
+import Accordion from 'react-native-collapsible/Accordion';
+import firebase from '../firebase'
+import List from './List'
 
 class TodoList extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        todo : '',
+        loadData : true,
+        todos : null
+      } 
+      this.renderHeader = this.renderHeader.bind(this)
+    }
+
+    componentDidMount() {
+      firebase.database().ref('todos/').on('value', (snapshot) => {
+        const todosFiles = snapshot.val();  
+        this.setState({todos: todosFiles, loadData: false})
+      })
+    }
+
+    renderHeader(todos) {
+      return (
+        <List list={todos} navigate={ this.props.navigate } />
+      );
+    }
+  
+    renderContent(todos) {
+      return (
+        <View style={styles.panel}>
+          <Text>{todos.description}</Text>
+        </View>
+      );
+    }
+  
+
     render() {
-        return (
-            <Card>
-              <Button
-                icon={<Icon name='add' color='#ffffff' />}
-                backgroundColor='#D50000'
-                buttonStyle={styles.buttonStyle}
-                title="Add Todo"
-                onPress={() => this.props.navigate.navigate("TodoPage")} />
-            </Card>
-        )
+      const { loadData } = this.state
+      const todos = !this.state.todos ? [] : Object.keys(this.state.todos).map( key => {
+        return {
+          key: key,
+          text: this.state.todos[key].todoTitle,
+          description: this.state.todos[key].todoDescription,
+        }})
+      return (
+        <ScrollView style={styles.container}>
+          <Card>
+            <Button
+              icon={<Icon name='add' color='#ffffff' />}
+              backgroundColor='#D50000'
+              buttonStyle={styles.buttonStyle}
+              title="Add Todo"
+              onPress={() => this.props.navigate.navigate("TodoPage")} />
+          </Card>
+           <Card title="DAFTAR TODO">
+            <Accordion
+              sections={todos}
+              renderHeader={this.renderHeader}
+              renderContent={this.renderContent}
+            />  
+          </Card>
+
+          {
+            loadData && <View style={styles.horizontal}>
+                          <ActivityIndicator size="large" color="#0000ff" />
+                        </View>
+          }
+          </ScrollView>
+      )
     }
 }  
 
